13909a607f7f82e39b5f02018735e39ff8f4ba76
defoxicator <104618176+defoxicator@users.noreply.github.com>
2024-06-06 10:30:09 +0200

    added boundaries visualization

--- code/app.py
+++ code/app.py
@@ -3,27 +3,39 @@ import unicodedata
 # STEP 0.
 # Encode characters in bytes
 
-text_input:str=input("Please insert text that shouldbe converted to QR Code:\n> ")
-
-def analyze_input(text_input=text_input):
-    
-# Encode unicode characters to binary
-    analyzed_text:list=list()
-
-    for character in text_input:
-        binary:str=bin(ord(character))[2:]
-
-        # make it in packets of 8 bits
-
-        analyzed_text.append({character:binary})
-
-    return(analyzed_text)
-
 # STEP 1.
 # Create data segment
 
+class Input:
+    def __init__(self):
+        self.text_input:str=input("Please insert text that shouldbe converted to QR Code:\n> ")
+        
+    def analyze_input(self):
+        
+# Encode unicode characters to binary
+        analyzed_text:list=list()
+
+        for character in text_input:
+            binary:str=bin(ord(character))[2:]
+
+# Make it in packets of 8 bits
+            while len(binary) < 8:
+                binary='0'+binary
+
+            analyzed_text.append({character:binary})
+
+        return(analyzed_text)
+
 # STEP 2.
 # Fit to version number
+
+class Version:
+# Versions as dict like
+# version_x_y:dict={number_of_bits:number_of_codewords}
+    def __init__(self):
+        version_1_9:dict={148:19}
+        version_10_26:dict={156:20}
+        version_27_40:dict={156:20}
 
 # STEP 3.
 # Concatenate segments, add padding, make codewords
@@ -33,6 +45,31 @@ def analyze_input(text_input=text_input):
 
 # STEP 5.
 # Draw fixed patterns
+
+class QrCode:
+    def __init__(self):
+# Size in col, rows
+        self.size=(21,21)
+        self.boundaries:list=[]
+    # Boundaries
+        for col in range(self.size[0]):
+            self.row_boundaries:list=[]
+
+            for row in range(self.size[1]):
+                self.row_boundaries.append("@")
+                        
+            self.boundaries.append(self.row_boundaries)
+
+# Timing pattern
+# Row 6, column 6
+    def draw_timing_pattern(self):
+        for row in self.size:
+            pass
+
+    def print_qr_code(self):
+        for col in self.boundaries:
+            print(col)
+
 
 # STEP 6.
 # Draw codewords and remainder
@@ -54,4 +91,4 @@ def analyze_input(text_input=text_input):
 # right towards top and then to the left
 
 if __name__ == "__main__":
-    print(analyze_input())
+    QrCode().print_qr_code()
